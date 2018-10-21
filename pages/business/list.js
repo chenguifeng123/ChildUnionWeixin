5ae9fbebdedb83f62a1fe1a32e3441f6c5493b33
chenguifeng <22255946@qq.com>
2018-10-21 22:26:01 +0800

    xiala

--- pages/business/list.js
+++ pages/business/list.js
@@ -72,11 +72,13 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
+    /*
     this.data.start = 1;
     wx.showToast({
       title: '正在刷新',
     });
     this.loadAllBusiness();
+    */
   },
 
   /**
